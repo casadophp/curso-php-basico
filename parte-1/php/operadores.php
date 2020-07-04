c242429afeb8f8a0a2829c62c6a0dd12fcf22fcd
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2020-07-04 17:26:09 -0300

    Estudos

---
+++ parte-1/php/operadores.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * ( + ) = Adição - (OK)
+ * ( - ) = Subtração
+ * ( * ) = Multiplicação
+ * ( / ) = Divisão
+ * ( % ) = Módulo - Especial
+ */
+
+// Adição
+echo "<br /> A ( soma ) de 5 + 5 é = ", 5 + 5;
+echo "<br /> A ( soma ) de 5.1 + 5.4 é = ", 5.10 + 5.40;
+
+// Subtração
+echo "<br /><br />A ( subtração ) de 2 - 1 é = ", 2 - 1;
+echo "<br />A ( subtração ) de 4 - 3 é = ", 4 - 3;
+
+// Multiplicar
+echo "<br /><br />A ( multiplicar ) de 2 * 2 é = ", 2 * 2;
+echo "<br />A ( multiplicar ) de 4 * 2 é = ", 4 * 2;
+
+// Divisão
+echo "<br /><br />A ( divisão ) de 6 / 2 é = ", 6 % 2;
+echo "<br />A ( divisão ) de 10 / 2 é = ", 10 % 2;
+
+// Resto
+echo "<br /><br /> Resto = ", 1 % 2; 
+echo "<br /> Resto = ", 2 % 2; 
+echo "<br /> Resto = ", 3 % 2; 
+echo "<br /> Resto = ", 4 % 2; 
+echo "<br /> Resto = ", 5 % 2; 
