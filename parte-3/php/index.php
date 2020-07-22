7e59cafa483b25660d4c884eea771671d1f3147f
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2020-07-22 19:26:43 -0300

    Ajuste do perfil

---
+++ parte-3/php/index.php
@@ -0,0 +1,33 @@
+<html>
+
+<body>
+  <h1 id="teste"></h1>
+
+  <script>
+  // document.getElementById("teste").innerHTML =
+  //   "Olá, mundo" + new Date();
+  /**
+   * ( + ) = Adição - (OK)
+   * ( - ) = Subtração
+   * ( * ) = Multiplicação
+   * ( / ) = Divisão
+   * ( % ) = Módulo - Especial
+   */
+  document.write("<br /> A ( soma ) de 5 + 5 é = ", 5 + 5);
+  document.write("<br /> A ( soma ) de 5.1 + 5.4 é = ", 5.10 + 5.40);
+
+  document.write("<br /> Subtração <br />");
+  document.write("<br /> A ( subtração ) de 2 - 1 é = ", 2 - 1);
+  document.write("<br /> A ( subtração ) de 4 - 3 é = ", 4 - 3);
+
+  document.write("<br /> Multiplicar <br />");
+  document.write("<br /> A ( multiplicar ) de 2 * 2 é = ", 2 * 2);
+  document.write("<br /> A ( multiplicar ) de 4 * 2 é = ", 4 * 2);
+
+  document.write("<br /> Divisão <br />");
+  document.write("<br /> A ( divisão ) de 6 / 2 é = ", 6 / 2);
+  document.write("<br /> A ( divisão ) de 10 / 2 é = ", 10 / 2);
+  </script>
+</body>
+
+</html>
---
+++ parte-3/php/variaveis.php
@@ -0,0 +1,31 @@
+<html>
+
+<body>
+  <script>
+  var teste = "teste";
+
+  function mostrarTeste() {
+
+    console.log(teste);
+  }
+  // teste = 1;
+  // console.log(teste);
+  // mostrarTeste();
+
+  // AQUI
+  //let outroTeste = "outro - teste";
+
+  function mostrarOutroTeste() {
+    let outroTeste = "teste";
+    outroTeste = "1";
+    //teste = 'Olá';
+    console.log(outroTeste);
+  }
+
+  // console.log(outroTeste);
+  // console.log(outroTeste);
+  mostrarOutroTeste();
+  </script>
+</body>
+
+</html>
