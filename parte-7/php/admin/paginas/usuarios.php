ef49f0f7ffbf95f39e2048a7e6f45fedbc1ec9d3
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2021-01-27 18:02:00 -0300

    Ajustes

--- parte-7/php/admin/paginas/usuarios.php
+++ parte-7/php/admin/paginas/usuarios.php
@@ -1,3 +1,43 @@
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
   <h1 class="h2">Listagem de usuários</h1>
 </div>
+
+<form>
+  <div class="row">
+
+    <div class="col">
+      <input required type="text" class="form-control" placeholder="Seu nome" aria-label="Seu Nome">
+    </div>
+
+    <div class="col">
+      <input required type="email" class="form-control" placeholder="Seu email" aria-label="Seu Email">
+    </div>
+  </div>
+
+  <div class="row mt-3">
+    <div class="col">
+      <input required type="text" class="form-control" placeholder="Usuário" aria-label="Usuário">
+    </div>
+
+    <div class="col">
+      <input required type="password" class="form-control" placeholder="Senha" aria-label="Senha">
+    </div>
+  </div>
+
+  <div class="row mt-3">
+    <div class="col">
+      <div class="form-floating">
+        <textarea class="form-control" placeholder="Digite sobre o intrevistador"></textarea>
+        <label for="floatingTextarea">Digite sobre o intrevistador</label>
+      </div>
+    </div>
+  </div>
+
+  <hr>
+
+  <div class="row">
+    <div class="col-12">
+      <button type="submit" class="btn btn-primary">Cadastrar o usuário</button>
+    </div>
+  </div>
+</form>
